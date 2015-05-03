158caf8cce7240a8966a70a2b44fc6d037145829
danigb <danigb@gmail.com>
2015-05-03 12:06:37 +0200

    New clone method. Map now works

--- test/score.js
+++ test/score.js
@@ -13,6 +13,15 @@ vows.describe('Score').addBatch({
       assert(score().time.sub, 4);
     }
   },
+  "length of score":  {
+    "returns the total length": function() {
+      var s = score("A | B");
+      assert.equal(s.length(), 2 * s.time.measure);
+    },
+    "no events length": function() {
+      assert.equal(score().length(), 0);
+    }
+  },
   "each": function() {
     var times = 0;
     var s = score('a b c d')
@@ -24,17 +33,21 @@ vows.describe('Score').addBatch({
   },
   "map": function() {
     var s1 = score('a b c d');
-    var s2 = s1.map(function(e) { return e; });
+    var s2 = s1.map(function(e) {
+      return e.set('value', e.value().toUpperCase());
+    });
     assert(s2 instanceof score);
+    assert.equal(s2.events.length, 4);
+    assert.equal(s2.events[0].value(), 'A')
+    assert.equal(s2.events[1].value(), 'B')
+    assert.equal(s2.events[2].value(), 'C')
+    assert.equal(s2.events[3].value(), 'D')
   },
-  "length":  {
-    "returns the total length": function() {
-      var s = score("A | B");
-      assert.equal(s.length(), 2 * s.time.measure);
-    },
-    "no events length": function() {
-      assert.equal(score().length(), 0);
+  "clone": function() {
+    var s1 = score('a b c d');
+    var s2 = s1.clone();
+    for(var i = 0; i < s1.events.length; i++) {
+      assert.equal(s2.events[i].value(), s1.events[i].value());
     }
   }
-
 }).export(module);
