0e5b006a397dbd4f91433496fa89f8c725a5206d
danigb <danigb@gmail.com>
2015-05-03 12:34:45 +0200

    Minor version

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
   Score.Event = require('./lib/event.js');
   Score.Time = require('./lib/time.js');
 
-  Score.version = "0.2.0";
   if (typeof define === "function" && define.amd) define(function() { return Score; });
   else if (typeof module === "object" && module.exports) module.exports = Score;
   else this.Score = Score;
