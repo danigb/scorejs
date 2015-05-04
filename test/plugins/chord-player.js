2adf4a3992fe48dbcb4293d5dad331e4b33240ce
danigb <danigb@gmail.com>
2015-05-04 23:15:09 +0200

    Chord player annotate chord and instrument

--- test/plugins/chord-player.js
+++ test/plugins/chord-player.js
@@ -13,10 +13,12 @@ vows.describe('Chord player').addBatch({
   },
   "play chords": function() {
     var chord = teoria.chord('Cmaj7');
-    var s = score('Cmaj7').playChords();
+    var s = score('Cmaj7').playChords({instrument: 'myInstrument'});
     assert.equal(s.events.length, chord.notes().length);
     s.events.forEach(function(e) {
       assert.equal(e.type, 'note');
+      assert.equal(e.instrument, 'myInstrument');
+      assert.equal(e.fromChord, 'Cmaj7');
     });
   }
 }).export(module);
