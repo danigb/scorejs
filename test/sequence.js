ed754d88962b898f1a853b1edf51d7908cbfc423
danigb <danigb@gmail.com>
2015-05-06 04:22:49 +0200

    Remove unused code

--- test/sequence.js
+++ test/sequence.js
@@ -4,7 +4,7 @@ var assert = require('assert');
 var seq = require('../lib/sequence.js')();
 
 vows.describe('Sequence').addBatch({
-  "constructor" : {
+  "sequence:constructor" : {
     "always return score instance": function() {
       assert(seq() instanceof seq);
     },
@@ -13,12 +13,12 @@ vows.describe('Sequence').addBatch({
       assert(seq().time.sub, 4);
     },
   },
-  "parse no plugins": function() {
+  "sequence:parse": function() {
     var s = seq('a b');
-    assert.equal(s.value(0), 'a');
-    assert.equal(s.value(1), 'b');
+    assert.equal(s.events[0].value(), 'a');
+    assert.equal(s.events[1].value(), 'b');
   },
-  "map": {
+  "sequence:map": {
     "returns a sequence": function() {
       var s = seq('a b').map(function(a) { return a; });
       assert(s instanceof seq);
@@ -45,22 +45,8 @@ vows.describe('Sequence').addBatch({
       assert.equal(s.events[0], s.events[1]);
     }
   },
-  "morph": function() {
-    var s = seq('a b').morph(function(event, newSeq) {
-      newSeq.add(event, {value: event.value().toUpperCase() });
-    });
-    assert.equal(s.events[0].value(), 'A');
-    assert.equal(s.events[1].value(), 'B');
-  },
-  "toString": function() {
+  "sequence:toString": function() {
     var s = seq('a b');
     assert.equal(s.toString(), 'a b');
-  },
-  "clone": function() {
-    var s1 = seq('a b c d');
-    var s2 = s1.clone();
-    for(var i = 0; i < s1.events.length; i++) {
-      assert.equal(s2.events[i].value, s1.events[i].value);
-    }
   }
 }).export(module);
