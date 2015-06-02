b9848fb604b15e665cc2934a11974edc96120e79
danigb <danigb@gmail.com>
2015-06-02 13:50:36 +0200

    Score Builder plugin

--- ext/builder.js
+++ ext/builder.js
@@ -1,17 +1,45 @@
 'use strict'
 
 module.exports = function (Score) {
-  Score.build = function (score) {
-    var scores = Object.keys(score).map(function (name) {
-      var part = score[name]
-      if (typeof part === 'string') {
-        return Score(part)
-      } else {
-        var source = part.score
-        delete part.score
-        return Score(source, part)
-      }
-    })
-    return Score.merge.apply(null, scores)
+  Score.build = function (obj) {
+    if (obj.parts) return buildScore(Score, obj)
+    else return mergeParts(Score, buildParts(Score, obj))
   }
+}
+
+function buildScore (Score, obj) {
+  var score = {}
+
+  score.parts = buildParts(Score, obj.parts)
+  merge(score, obj, 'parts')
+  return score
+}
+
+function mergeParts (Score, parts) {
+  var values = Object.keys(parts).map(function (name) { return parts[name] })
+  return Score.merge.apply(null, values)
+}
+
+function buildParts (Score, parts) {
+  var parsed = {}
+  Object.keys(parts).forEach(function (name) {
+    var part = parts[name]
+    if (typeof part === 'string') {
+      parsed[name] = Score(part)
+    } else {
+      var source = part.score
+      var options = merge({}, part, 'score')
+      parsed[name] = Score(source, options)
+    }
+  })
+  return parsed
+}
+
+function merge (dest, src, skip) {
+  for (var n in src) {
+    if (n !== skip) {
+      dest[n] = src[n]
+    }
+  }
+  return dest
 }
--- test/builder.js
+++ test/builder.js
@@ -15,6 +15,22 @@ vows.describe('Score builder').addBatch({
     assert.deepEqual(_.pluck(s.sequence, 'position'), [0, 0, 0.5, 0.5])
     assert.deepEqual(_.pluck(s.sequence, 'duration'), [0.5, 0.5, 0.5, 0.5])
   },
+  'builder with parts': function() {
+    var s = Score.build({
+      title: 'The Title',
+      time: '2/4',
+      parts: {
+        melody: {
+          score: 'e b |',
+          transpose: 'M2'
+        },
+        chords: 'C G |'
+      }
+    })
+    assert.equal(s.title, 'The Title')
+    assert.equal(s.time, '2/4')
+    assert.deepEqual(_.pluck(s.parts['melody'].sequence, 'value'), ['f#2', 'c#3'])
+  },
   'builder with transformation': function () {
     var s = Score.build({
       melody: {
